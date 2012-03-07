c2a8f78172ca119a851d7f6d8d8744bbce37d6d3
Vignesh R E <vignesh.littlemaster@gmail.com>
2012-03-07 10:27:15 +0530

    Added Dev Seeds<v2>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+def orphanage_valid_attributes
+  {:name => "orphanage", :nature => "old age", :address => "address 1", :city => "bangalore", :manager_name => "mgr", :contact_number => "0807766554", :account_details => "sbi acc", :email => "email@address.com"}
+end
+
+def need_valid_attributes
+  {:description => "a" * 250, :nature => "food related", :severity => "critical", :deadline => Date.today}
+end
+
+puts "Deleting all needs.."
+Need.delete_all
+puts "Deleting all orphanages.."
+Orphanage.delete_all
+
+puts "Loading first 20 bangalore orphanages.."
+(1..20).each do |i|
+  Orphanage.create(orphanage_valid_attributes.merge(:name => "orphanage_#{i}"))
+end
+
+puts "Loading second 20 chennai orphanages.."
+(21..40).each do |i|
+  Orphanage.create(orphanage_valid_attributes.merge(:name => "orphanage_#{i}", :city => "chennai"))
+end
+
+puts "Updating secret passwords of orphanages to password.."
+Orphanage.all.each do |orphanage|
+  orphanage.update_attributes({:secret_password => "password"}, :as => :admin)
+end
+
+puts "Creating 10 needs each for first two orphanages.."
+orphanages = Orphanage.all(:limit => 2)
+orphanages.each do |orphanage|
+  (1..10).each do |i|
+    need = Need.new(need_valid_attributes)
+    need.deadline = Date.today + i
+    need.orphanage_id = orphanage.id
+    need.save
+  end
+end
